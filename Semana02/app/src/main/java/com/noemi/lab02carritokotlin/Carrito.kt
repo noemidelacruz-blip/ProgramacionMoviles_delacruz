f71763e908b7e93542a29358bac3612592234d16
noemidelacruz-blip <noemi.delacruz@tecsup.edu.pe>
2026-08-24 22:26:13 -0500

    Proyecto inicial con menu del carrito

---
+++ Semana02/app/src/main/java/com/noemi/lab02carritokotlin/Carrito.kt
@@ -0,0 +1,11 @@
+package com.noemi.lab02carritokotlin
+
+fun main(){
+    println("==================================")
+    println("CARRITO DE COMPRAS TIENDA TECSUP")
+    println("==================================")
+
+}
+class Carrito {
+
+}
