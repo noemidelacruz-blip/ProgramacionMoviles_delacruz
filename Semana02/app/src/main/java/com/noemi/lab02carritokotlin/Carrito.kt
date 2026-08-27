39f9093f3ff9f72f136d383c44646cd64fab41f1
noemidelacruz-blip <noemi.delacruz@tecsup.edu.pe>
2026-08-26 22:57:37 -0500

    Agrega data class Producto y funciones de subtotal, IGV y total

--- Semana02/app/src/main/java/com/noemi/lab02carritokotlin/Carrito.kt
+++ Semana02/app/src/main/java/com/noemi/lab02carritokotlin/Carrito.kt
@@ -1,11 +1,53 @@
 package com.noemi.lab02carritokotlin
 
+data class Producto(
+    val nombre: String,
+    val precio: Double,
+    var cantidad: Int
+)
+
 fun main(){
-    println("==================================")
+    println("====================================")
     println("CARRITO DE COMPRAS TIENDA TECSUP")
-    println("==================================")
+    println("====================================")
+    val nombreCliente = "Noemi De La Cruz"
+    val carrito = mutableListOf<Producto>()
+    println("Cliente: $nombreCliente")
+    println()
 
+    carrito.add(Producto("Laptop HP", 2500.0, 1))
+    carrito.add(Producto("Mouse Logitech", 45.5, 2))
+    carrito.add(Producto("Audifonos Sony", 120.0, 1))
+
+    for (producto in carrito) {
+        println("Producto agregado: ${producto.nombre}")
+    }
+    println()
+
+    val subtotal = calcularSubtotal(carrito)
+    val igv = calcularIGV(subtotal)
+    val total = calcularTotal(subtotal, igv)
+
+
+    println(String.format("Subtotal  : S/ %.2f", subtotal))
+    println(String.format("IGV (18%%): S/ %.2f", igv))
+    println(String.format("TOTAL     : S/ %.2f", total))
 }
-class Carrito {
+fun calcularSubtotal(productos:List<Producto>): Double {
+    var subtotal = 0.0
+    for(p in productos){
+        subtotal += p.precio *   p.cantidad
+    }
+    return subtotal
+}
+fun calcularIGV(subtotal: Double): Double {
+    // TODO: devuelve el 18% del subtotal
+    var igv = 0.18
+    return subtotal * igv
+}
+fun calcularTotal(subtotal: Double, igv: Double): Double {
+    // TODO: devuelve la suma de ambos
+    val precioTotal = subtotal + igv
 
+    return precioTotal
 }
