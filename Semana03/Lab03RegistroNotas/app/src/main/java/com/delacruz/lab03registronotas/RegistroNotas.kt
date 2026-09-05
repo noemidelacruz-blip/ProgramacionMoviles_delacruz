db982fb1bb72e4124214461ec9f95f8843e974c6
noemidelacruz <noemi.delacruz@tecsup.edu.pe>
2026-09-05 14:07:22 -0500

    Inicializar proyecto RegistroDeNotas en el repositorio (Tarea 2)

---
+++ Semana03/Lab03RegistroNotas/app/src/main/java/com/delacruz/lab03registronotas/RegistroNotas.kt
@@ -0,0 +1,23 @@
+package com.delacruz.lab03registronotas
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RegistroNotas() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            // Estructura base limpia
+        }
+    }
+}
