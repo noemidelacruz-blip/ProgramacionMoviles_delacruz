bdd310d2d538267e32ddf73f7d6eb6f586e1650d
noemidelacruz <noemi.delacruz@tecsup.edu.pe>
2026-09-22 17:19:26 -0500

    Estructura base de NavLab traida desde sin-ia
    Incluye configuracion de Gradle, Jetpack Navigation (AppNavigation, Screen) y pantallas iniciales (HomeScreen, DetailScreen, ListScreen, ProfileScreen).

---
+++ Semana05/app/src/main/java/com/delacruz/navlab/navigation/AppNavigation.kt
@@ -0,0 +1,44 @@
+package com.delacruz.navlab.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.delacruz.navlab.screens.DetailScreen
+import com.delacruz.navlab.screens.HomeScreen
+import com.delacruz.navlab.screens.ListScreen
+import com.delacruz.navlab.screens.ProfileScreen
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(Screen.Home.route) {
+            HomeScreen(navController)
+        }
+        composable(Screen.List.route) {
+            ListScreen(navController)
+        }
+        composable(Screen.Profile.route) {
+            ProfileScreen(navController)
+        }
+        composable(
+            route = Screen.Detail.route,
+            arguments = listOf(
+                navArgument("itemId") {
+                    type = NavType.IntType
+                    defaultValue = 0
+                }
+            )
+        ) { backStackEntry ->
+            val itemId = backStackEntry.arguments?.getInt("itemId") ?: 0
+            DetailScreen(navController, itemId)
+        }
+    }
+}
