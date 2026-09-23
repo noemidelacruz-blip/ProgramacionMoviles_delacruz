e11e0fbd3d4a5198e61072d9238bffa7d8ae3ab8
noemidelacruz <noemi.delacruz@tecsup.edu.pe>
2026-09-23 08:02:05 -0500

    Commit 01: Inicialización del proyecto ClinicaSalud en Semana05 con dependencias de Jetpack Compose y librería de iconos extendidos

---
+++ Semana05/ClinicaSalud/app/src/main/java/com/delacruz/clinicasalud/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.delacruz.clinicasalud
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                PantallaPerfilDoctor()
+            }
+        }
+    }
+}
